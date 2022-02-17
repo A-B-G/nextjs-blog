7be6cd1d0c0438796d571de44c2dbc93150d7445
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2022-02-17 10:00:08 -0800

    changed utilRequests file extension

---
+++ pages/utilRequests.ts
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+
+  //create a get request to import elsewhere
+export async function getAllPosts() {
+    //send get request and wait for response to resolve, then save response to variable
+    let res = await fetch("https://test-blog-db.herokuapp.com/test");
+    //process response object with .json() after promise resolves and save it to a variable
+    let resJson = await res.json();
+    //save resolved res.json() object to variable 
+    let dbData = await resJson;
+    //return the db data 
+    return dbData;
+
+}
