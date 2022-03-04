48150782ce69fb23c4545728c6aa55bfec5bd415
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2022-03-04 11:59:04 -0800

    added fn to send request with access token after login authenticated

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,17 +1,13 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import utilStyles from '../styles/utils.module.css';
-import { getAllPosts } from '../pages/utilRequests';
+import { getAllPosts, requestLogin, createNewBlog } from '../pages/utilRequests';
+import { MyPostsDataProps } from '../interfaceTypes';
+import { initialVal } from '../hooks/useInputState';
+// import FormInputHook from '../hooks/FormInputHook';
 
-type MyPostsDataProps = {
-    allPostsData: {
-        id: number;
-        date: string;
-        modified?: string;
-        title: string;
-        excerptpost: string;
-        fullpost: string
-    }[]
-  }
+
+
 // process the data from getAllPosts INSIDE getStaticProps
 export async function getStaticProps() {
     const allPostsData = await getAllPosts();
@@ -21,9 +17,24 @@ export async function getStaticProps() {
     }
 }
 
-
 export default function Login({ allPostsData }: MyPostsDataProps) {
 
+    let [username, setUsername, resetUsername] = initialVal(" ");
+    let [password, setPassword, resetPassword] = initialVal(" ");
+    let [token, setToken] = useState("");
+
+    const handleSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        let loginData = { username, password }
+        requestLogin(loginData)
+        .then((val) => {
+            setToken(val);
+            // console.log("token:", token);
+            createNewBlog(token);
+        })
+        resetUsername(" ");
+        resetPassword(" ");
+    }
     return (
         <div>
             <Head>
@@ -37,11 +48,32 @@ export default function Login({ allPostsData }: MyPostsDataProps) {
             </header>
 
             <main className={utilStyles.main}>
-                <label>name</label>
-                <input type="text" placeholder="name" />
+                {/* <FormInputHook                 
+                 /> */}
+                 <div className="login-form">
+                    <fieldset>
+                    <label htmlFor="username">Username</label>
+                    <input 
+                        type="text"
+                        placeholder="username"
+                        name="username"
+                        value={username}
+                        onChange={setUsername}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="text"
+                        placeholder="password"
+                        name="password"
+                        value={password}
+                        onChange={setPassword}
+                    />
+                    <button onClick={handleSubmit}>Login</button>
+                    </fieldset>
+                 </div>
                 <ul className={utilStyles.list}>
                     {allPostsData.map((d => (
-                        <li>
+                        <li key={d.title}>
                             <h2>{d.title}</h2>
                             <article>{d.excerptpost}</article>
                         </li>
